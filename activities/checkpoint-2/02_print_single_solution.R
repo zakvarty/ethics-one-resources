77ddfd8a51550238b4a021b4a44d4fec717f9bb1
Zak D Varty <44709288+zakvarty@users.noreply.github.io>
2023-11-09 17:09:23 +0000

    move old checkpoint before quartofying

--- activities/checkpoint-2/02_print_single_solution.R
+++
@@ -1,6 +0,0 @@
-# Load to answer each part of the assessment ---------------------------
-source("./00_checkpoint_2_helpers.R")
-bank_data <- readRDS("./checkpoint_2_data/bank_data.RDS")
-
-ID <- 1 # integer between 1 and 50.
-print_solutions(ID)
